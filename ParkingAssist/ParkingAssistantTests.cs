2ae3a5950a64def7f876df10bebbe93ede94e255
Stephen Gowen <sjgowen@gmail.com>
2021-08-15 12:51:01 +0100

    Failing test to park Prop in empty Airport

--- ParkingAssist/ParkingAssistantTests.cs
+++ ParkingAssist/ParkingAssistantTests.cs
@@ -6,9 +6,12 @@ namespace ParkingAssist
     public class ParkingAssistantTests
     {
         [Fact]
-        public void Test1()
+        public void ParkPropInEmptyAirportAllocatedStand00()
         {
-
+            Stands stands = new Stands(25, 50, 25);
+            Airplane airplane = new Airplane("TST-0001", "KLM", "Prop", 180);
+            string stand = airplane.Park(stands);
+            Assert.Equal("00", stand);
         }
     }
 }
