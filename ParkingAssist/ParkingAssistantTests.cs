d5da0760447d2e2be3de9bbffbbdc7d94722ed9a
Stephen Gowen <sjgowen@gmail.com>
2021-08-15 12:34:04 +0100

    Add project files.

---
+++ ParkingAssist/ParkingAssistantTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ParkingAssist
+{
+    public class ParkingAssistantTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
