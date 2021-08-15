b3c2aaf3bf42f601c8e8ec7bf70a0542558a8142
Stephen Gowen <sjgowen@gmail.com>
2021-08-15 15:57:01 +0100

    Park a Jet in an empty Airport gets put in Stand 25

--- ParkingAssist/ParkingAssistantTests.cs
+++ ParkingAssist/ParkingAssistantTests.cs
@@ -15,6 +15,15 @@ namespace ParkingAssist
             int stand = stands.Park(airplane, DateTime.Now);
             Assert.Equal(0, stand);
         }
+
+        [Fact]
+        public void ParkJetInEmptyAirportAllocatedStand25()
+        {
+            AircraftStands stands = new(25, 50, 25);
+            Airplane airplane = new("TST-0002", "RYA", PlaneSize.Jet, 180);
+            int stand = stands.Park(airplane, DateTime.Now);
+            Assert.Equal(25, stand);
+        }
     }
 }
 
